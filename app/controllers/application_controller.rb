efa8e1c9e2cea32059177c39c7eb7fa915e21274
Thynkon <thynkon@protonmail.com>
2025-05-21 13:44:43 +0200

    Initial commit with JWT authentication

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+class ApplicationController < ActionController::API
+  before_action :authorized
+  def encode_token(payload)
+        JWT.encode(payload, Rails.application.credentials.jwt_hmac!, "HS256")
+  end
+
+    def decoded_token
+        header = request.headers["Authorization"]
+        if header
+            token = header.split(" ")[1]
+            begin
+              JWT.decode(token, Rails.application.credentials.jwt_hmac!)
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+  def current_user
+        if decoded_token
+            user_id = decoded_token[0]["user_id"]
+            @user = User.find_by(id: user_id)
+        end
+  end
+
+    def authorized
+        unless !!current_user
+          render json: { message: "Please log in" }, status: :unauthorized
+        end
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+require "argon2id"
+
+# Schema: User(name: string, password_digest:string)
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, uniqueness: true
+  validates :password_digest, presence: true
+
+  def password=(unencrypted_password)
+    if unencrypted_password.nil?
+      @password = nil
+      self.password_digest = nil
+    elsif !unencrypted_password.empty?
+      @password = unencrypted_password
+      self.password_digest = Argon2id::Password.create(unencrypted_password)
+    end
+  end
+
+  def authenticate(unencrypted_password)
+    password_digest? && Argon2id::Password.new(password_digest).is_password?(unencrypted_password) && self
+  end
+
+  def password_salt
+    Argon2id::Password.new(password_digest).salt if password_digest?
+  end
+end
