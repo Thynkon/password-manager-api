095ed54a5913a2512b54ec26bf2cfdb04dbf2092
Thynkon <thynkon@protonmail.com>
2025-05-28 12:02:51 +0200

    Fix CORS and verify expiration for JWT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,31 @@
 class ApplicationController < ActionController::API
   before_action :authorized
   def encode_token(payload)
-        JWT.encode(payload, Rails.application.credentials.jwt_hmac!, "HS256")
+    now = Time.now.to_i
+    payload[:iat] = now
+    payload[:exp] = now + (24 * 60 * 60)
+
+    JWT.decode(token, Rails.application.credentials.jwt_hmac!, true, { algorithm: "HS256" })
   end
 
-    def decoded_token
-        header = request.headers["Authorization"]
-        if header
-            token = header.split(" ")[1]
-            begin
-              JWT.decode(token, Rails.application.credentials.jwt_hmac!)
-            rescue JWT::DecodeError
-                nil
-            end
-        end
+  def decoded_token
+    header = request.headers["Authorization"]
+    if header
+      token = header.split(" ")[1]
+      begin
+        JWT.decode(
+          token,
+          Rails.application.credentials.jwt_hmac!,
+          true,
+          { algorithm: "HS256" }
+        )
+      rescue JWT::ExpiredSignature
+        render json: { message: "Token has expired" }, status: :unauthorized
+      rescue JWT::DecodeError
+        render json: { message: "Invalid token" }, status: :unauthorized
+      end
     end
+  end
 
   def current_user
         if decoded_token
